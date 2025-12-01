e84e1e01d41123f63f30f052d71e7c30be03f8a1
sharp-lilac <jessicacboles@gmail.com>
2025-12-01 12:44:01 -0600

    Add draft script structure to data_restructuring.r

--- data_restructuring.r
+++ data_restructuring.r
@@ -7,3 +7,9 @@ df_input_benthic_transects <- read_excel(filepath_benthic, sheet = 3, skip = 1)
 df_input_benthic_cover <- read_excel(filepath_benthic, sheet = 4, skip = 1)
 df_input_benthic_quadrats <- read_excel(filepath_benthic, sheet = 5, skip = 1)
 df_input_benthic_recruits <- read_excel(filepath_benthic, sheet = 6, skip = 1)
+
+# Covert AGRRA formatted data to GOAP formatted data ---------------------------
+
+
+
+# Prepare formatted data for data validation ---------------------------
