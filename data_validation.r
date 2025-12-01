7b7711e10b4614bfe873612a061af5e04943c9c7
sharp-lilac <jessicacboles@gmail.com>
2025-12-01 12:39:10 -0600

    Add draft script structure to data_validation.r

--- data_validation.r
+++ data_validation.r
@@ -4,3 +4,26 @@
 source("helper_scripts/packages_load.r")
 source("helper_scripts/functions_define.r")
 source("data_restructuring.r")
+
+# Define vectors of required columns ---------------------------
+columns_benthic_sites <- c(
+    "EA Period", "EAA Code", "Site ID", "Depth", "Latitude", "Longitude", "MPA", "Management",
+    "Protection Status", "Reef Zone", "Reef Type", "Visibility (ft)", "Weather"
+)
+# add benthic cover
+# add benthic quadrat
+# add benthic invert
+# add coral sites
+# add coral cover
+# add coral counts
+
+# Validate Benthic Sites ---------------------------
+# check_completeness(df_benthic_sites, columns_benthic_sites, "Benthic Sites")
+# e.g., grouping type: check_grouping(df_benthic_sites$`Protection Status`, c("None", "NTZ", "CUZ", "GUZ", "PUZ"), "Protection Status")
+# e.g., range type: check_range(df_benthic_sites$`Visibility (ft)`, 1, 50, "Visibility", "int")
+
+# Validate Benthic Cover ---------------------------
+
+# Validate Benthic Quadrats ---------------------------
+
+# etc.
