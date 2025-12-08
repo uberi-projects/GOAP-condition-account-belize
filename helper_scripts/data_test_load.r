62da3bd382c834f637128b116d00e08cb1c3437b
sharp-lilac <jessicacboles@gmail.com>
2025-12-08 11:44:22 -0600

    Ensure NAs are properly categorized during test data loading

--- helper_scripts/data_test_load.r
+++ helper_scripts/data_test_load.r
@@ -4,8 +4,8 @@
 source("helper_scripts/packages_load.r")
 
 ## Read test data ------------------------
-df_test_sites <- read_excel("data_dummy/Dummy data_OA_Benthic Data Template.xlsx", sheet = 5, nas = "NA")
-df_test_benthic_cover <- read_excel("data_dummy/Dummy data_OA_Benthic Data Template.xlsx", sheet = 6, nas = "NA")
-df_test_recruits <- read_excel("data_dummy/Dummy data_OA_Benthic Data Template.xlsx", sheet = 7,  nas = "NA")
-df_test_invertebrates <- read_excel("data_dummy/Dummy data_OA_Benthic Data Template.xlsx", sheet = 8,  nas = "NA")
-df_test_organisms <- read_excel("data_dummy/Dummy data_OA_Benthic Data Template.xlsx", sheet = 3,  nas = "NA")
+df_test_sites <- read_excel("data_dummy/Dummy data_OA_Benthic Data Template.xlsx", sheet = 5, na = "NA")
+df_test_benthic_cover <- read_excel("data_dummy/Dummy data_OA_Benthic Data Template.xlsx", sheet = 6, na = "NA")
+df_test_recruits <- read_excel("data_dummy/Dummy data_OA_Benthic Data Template.xlsx", sheet = 7, na = "NA")
+df_test_invertebrates <- read_excel("data_dummy/Dummy data_OA_Benthic Data Template.xlsx", sheet = 8, na = "NA")
+df_test_organisms <- read_excel("data_dummy/Dummy data_OA_Benthic Data Template.xlsx", sheet = 3, na = "NA")
