c9c9fcf434883dc017757789526ebbdefb281ef7
sharp-lilac <jessicacboles@gmail.com>
2025-12-09 13:37:53 -0600

    Fix typo in coral community data load

--- helper_scripts/data_test_load.r
+++ helper_scripts/data_test_load.r
@@ -10,5 +10,5 @@ df_test_recruits <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.x
 df_test_invertebrates <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = 8, na = "NA")
 df_test_organisms <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = 3, na = "NA")
 df_test_substrate <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = 2, na = "NA")
-df_test_coral_commuity <- read_excel("data_dummy/Dummy_data_OA_Coral Data Template.xlsx", sheet = 6, na = "NA")
+df_test_coral_community <- read_excel("data_dummy/Dummy_data_OA_Coral Data Template.xlsx", sheet = 6, na = "NA")
 df_test_fish <- read_excel("data_dummy/Dummy_data_OA_Relief Data Template.xlsx", sheet = 3, na = "NA")
