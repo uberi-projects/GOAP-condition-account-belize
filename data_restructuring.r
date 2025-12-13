3924630d6a4a35679a1970382141234eac8f35b2
sharp-lilac <jessicacboles@gmail.com>
2025-12-13 11:18:00 -0600

    Add support to read AGRRA data exports

--- data_restructuring.r
+++ data_restructuring.r
@@ -1,12 +1,25 @@
 # data_restructuring.r
 
 # Read AGRRA data as exported from AGRRA platform ---------------------------
-filename_benthic <- "BenthicRaw.xlsx" # change this value if your filename is different
-filepath_benthic <- file.path("data_deposit", filename_benthic)
-df_input_benthic_transects <- read_excel(filepath_benthic, sheet = 3, skip = 1)
-df_input_benthic_cover <- read_excel(filepath_benthic, sheet = 4, skip = 1)
-df_input_benthic_quadrats <- read_excel(filepath_benthic, sheet = 5, skip = 1)
-df_input_benthic_recruits <- read_excel(filepath_benthic, sheet = 6, skip = 1)
+filepath_benthic <- file.path("data_deposit", "BenthicRaw.xlsx")
+filepath_coral <- file.path("data_deposit", "CoralRaw.xlsx")
+filepath_fish <- file.path("data_deposit", "FishRaw.xlsx")
+filepath_metadata <- file.path("data_deposit", "Metadata.xlsx")
+df_benthic_transects <- read_excel(filepath_benthic, sheet = 3, skip = 1)
+df_benthic_cover <- read_excel(filepath_benthic, sheet = 4, skip = 1)
+df_invertebrates <- read_excel(filepath_benthic, sheet = 5, skip = 1)
+df_recruits <- read_excel(filepath_benthic, sheet = 6, skip = 1)
+df_coral_community_transects <- read_excel(filepath_coral, sheet = 3)
+df_coral_community <- read_excel(filepath_coral, sheet = 4, skip = 3)
+df_coral_community_diseases <- read_excel(filepath_coral, sheet = 5)
+df_coral_community_counts <- read_excel(filepath_coral, sheet = 6)
+df_fish <- read_excel(filepath_fish, sheet = 3, skip = 1)
+df_sites <- read_excel(filepath_metadata, sheet = 3)
+df_organisms <- read_excel(filepath_metadata, sheet = 6)
+df_organisms_group <- read_excel(filepath_metadata, sheet = 5)
+df_coralspp <- read_excel(filepath_metadata, sheet = 7)
+# df_substrate - not present in AGRRA export
+# df_disease - not present in AGRRA export
 
 # Covert AGRRA formatted data to GOAP formatted data ---------------------------
 
