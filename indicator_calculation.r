8321245adb8d2ee5f9b60ef74a1e1b427cf237e1
sharp-lilac <jessicacboles@gmail.com>
2025-12-14 06:20:59 -0600

    Define required indicators

--- indicator_calculation.r
+++ indicator_calculation.r
@@ -1,1 +1,11 @@
 # indicator_calculation.r
+
+# Source scripts ---------------------------
+source("data_validation.r")
+
+# Define required indicators ---------------------------
+indicator_lcc_include <- TRUE # live coral cover
+indicator_mac_include <- TRUE # macroalgae cover
+indicator_red_include <- TRUE # recruit density
+indicator_lcd_include <- TRUE # live coral diversity
+indicator_rug_include <- TRUE # rugosity
