39b195d7384de9b928ddbf4f4791e7b813324258
sharp-lilac <jessicacboles@gmail.com>
2025-12-15 06:33:19 -0600

    Load test data based on sheet name not sequence

--- helper_scripts/data_test_load.r
+++ helper_scripts/data_test_load.r
@@ -4,16 +4,16 @@
 source("helper_scripts/packages_load.r")
 
 ## Read test data ------------------------
-df_test_sites <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = 5, na = "NA")
-df_test_benthic_cover <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = 6, na = "NA")
-df_test_recruits <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = 7, na = "NA")
-df_test_invertebrates <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = 8, na = "NA")
-df_test_organisms <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = 3, na = "NA")
-df_test_substrate <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = 2, na = "NA")
-df_test_coral_community <- read_excel("data_dummy/Dummy_data_OA_Coral Data Template.xlsx", sheet = 6, na = "NA")
-df_test_coralspp <- read_excel("data_dummy/Dummy_data_OA_Coral Data Template.xlsx", sheet = 3, na = "NA")
-df_test_disease <- read_excel("data_dummy/Dummy_data_OA_Coral Data Template.xlsx", sheet = 2, na = "NA")
-df_test_fish <- read_excel("data_dummy/Dummy_data_OA_Relief Data Template.xlsx", sheet = 4, na = "NA")
+df_test_sites <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = "Sites", na = "NA")
+df_test_benthic_cover <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = "Benthic_Cover", na = "NA")
+df_test_recruits <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = "Recruits", na = "NA")
+df_test_invertebrates <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = "Invertebrates", na = "NA")
+df_test_organisms <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = "Ref_Organism", na = "NA")
+df_test_substrate <- read_excel("data_dummy/Dummy_data_OA_Benthic Data Template.xlsx", sheet = "Ref_Substrate", na = "NA")
+df_test_coral_community <- read_excel("data_dummy/Dummy_data_OA_Coral Data Template.xlsx", sheet = "Coral_Community", na = "NA")
+df_test_coralspp <- read_excel("data_dummy/Dummy_data_OA_Coral Data Template.xlsx", sheet = "Ref_Coral", na = "NA")
+df_test_disease <- read_excel("data_dummy/Dummy_data_OA_Coral Data Template.xlsx", sheet = "Ref_Disease", na = "NA")
+df_test_fish <- read_excel("data_dummy/Dummy_data_OA_Relief Data Template.xlsx", sheet = "Relief", na = "NA")
 
 ## Ensure time columns are properly formatted
 df_test_benthic_cover$Time <- format(df_test_benthic_cover$Time, "%H:%M")
