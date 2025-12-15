0ba4d7020de444c8cf16c978a799cfab9efe355a
sharp-lilac <jessicacboles@gmail.com>
2025-12-15 09:20:03 -0600

    Calculate live coral cover

--- indicator_calculation.r
+++ indicator_calculation.r
@@ -16,11 +16,29 @@ benthic_cover_presence <- df_benthic_cover %>%
     left_join(df_organisms_unique %>% select(Code, Primary_Type = Type), by = c("Organism" = "Code")) %>%
     left_join(df_organisms_unique %>% select(Code, Secondary_Type = Type), by = c("Secondary" = "Code")) %>%
     mutate(
-        Coral_Presence = Vectorize(calculate_type_weight)(Primary_Type, Secondary_Type, "Coral")
+        Coral_Presence = Vectorize(calculate_type_weight)(Primary_Type, Secondary_Type, "Coral"),
+        Year = format(Date, format = "%Y")
     )
 
 # Calculate live coral cover ---------------------------
-
+benthic_cover_presence_lcc <- benthic_cover_presence %>%
+    group_by(Year, Site, Transect) %>%
+    mutate(Coral_Cover_Tran = 100 * sum(Coral_Presence) / n()) %>%
+    group_by(Year, Site) %>%
+    mutate(Coral_Cover_Site = mean(Coral_Cover_Tran))
+indicator_lcc <- benthic_cover_presence_lcc %>%
+    group_by(Year) %>%
+    summarize(
+        `Min (Site)` = min(Coral_Cover_Site),
+        `Av. (Site)` = mean(Coral_Cover_Site),
+        `Median (Site)` = median(Coral_Cover_Site),
+        `Max (Site)` = max(Coral_Cover_Site),
+        `Min (Transect)` = min(Coral_Cover_Tran),
+        `Av. (Transect)` = mean(Coral_Cover_Tran),
+        `Median (Transect)` = median(Coral_Cover_Tran),
+        `Max (Transect)` = max(Coral_Cover_Tran)
+    ) %>%
+    mutate(across(-Year, ~ round(.x, 2)))
 
 # Calculate macroalgae cover ---------------------------
 
