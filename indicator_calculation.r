398bf7092c58872fd8eae21eb600d70ae4aff097
sharp-lilac <jessicacboles@gmail.com>
2025-12-16 16:40:41 -0600

    Calculate macroalgae cover indicator

--- indicator_calculation.r
+++ indicator_calculation.r
@@ -1,13 +1,14 @@
 # indicator_calculation.r
 
 # Set boolean for whether test data or AGRRA data should be used ---------------------------
-test_on <- TRUE
+test_on <- FALSE
 
 # Source scripts ---------------------------
 source("data_validation.r")
 
 # Set vectors ---------------------------
 coral_category <- ifelse(test_on == TRUE, "Coral", "Calcifiers :: Coral")
+algae_category <- ifelse(test_on == TRUE, "Fleshy Macroalgae", "Algae :: Macro :: Fleshy")
 
 # Prepare data ---------------------------
 df_organisms_unique <- df_organisms %>% distinct(Code, Type)
@@ -16,6 +17,7 @@ benthic_cover_presence <- df_benthic_cover %>%
     left_join(df_organisms_unique %>% select(Code, Secondary_Type = Type), by = c("Secondary" = "Code")) %>%
     mutate(
         Coral_Presence = Vectorize(calculate_type_weight)(Primary_Type, Secondary_Type, coral_category),
+        Algae_Presence = Vectorize(calculate_type_weight)(Primary_Type, Secondary_Type, algae_category),
         Year = format(as.Date(Date), format = "%Y")
     )
 
@@ -40,7 +42,24 @@ indicator_lcc <- benthic_cover_presence_lcc %>%
     mutate(across(-Year, ~ round(.x, 2)))
 
 # Calculate macroalgae cover ---------------------------
-
+benthic_cover_presence_fma <- benthic_cover_presence %>%
+    group_by(Year, Site, Transect) %>%
+    mutate(Algae_Cover_Tran = 100 * sum(Algae_Presence) / n()) %>%
+    group_by(Year, Site) %>%
+    mutate(Algae_Cover_Site = mean(Algae_Cover_Tran))
+indicator_fma <- benthic_cover_presence_fma %>%
+    group_by(Year) %>%
+    summarize(
+        `Min (Site)` = min(Algae_Cover_Site),
+        `Av. (Site)` = mean(Algae_Cover_Site),
+        `Median (Site)` = median(Algae_Cover_Site),
+        `Max (Site)` = max(Algae_Cover_Site),
+        `Min (Transect)` = min(Algae_Cover_Tran),
+        `Av. (Transect)` = mean(Algae_Cover_Tran),
+        `Median (Transect)` = median(Algae_Cover_Tran),
+        `Max (Transect)` = max(Algae_Cover_Tran)
+    ) %>%
+    mutate(across(-Year, ~ round(.x, 2)))
 
 # Calculate recruit density ---------------------------
 
